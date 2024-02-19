d499e9f1fd4e4064b0273a75434a9731cdb09cf5
Grilindor <marcor91@hotmail.fr>
2024-02-19 13:01:16 +0000

    task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,10 +3,10 @@
  *main - entry point
  *Description: the () are empty because i don't have any value to work with
  *with puts i make a new line at the and of my prints
- *return tell me with a 0 if the code work
+ *Return: tell me with a 0 if the code work
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
