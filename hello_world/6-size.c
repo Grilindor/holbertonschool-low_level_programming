263eae4fefa4cda963a948bb4039716687e9a370
Grilindor <marcor91@hotmail.fr>
2024-02-19 13:48:06 +0000

    task6

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: see
+ *Return: tell me with a 0 if the code work
+ */
+int main(void)
+{
+printf("Size of a char %lu byte(s)\n", sizeof(char));
+printf("Size of a int %lu byte(s)\n", sizeof(int));
+printf("Size of a long int %lu byte(s)\n", sizeof(int));
+printf("Size of a long long int %lu byte(s)\n", sizeof(int));
+printf("Size of a float %lu byte(s)\n", sizeof(float));
+return (0);
+}
