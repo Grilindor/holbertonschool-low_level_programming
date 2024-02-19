b7d003b75285a0da5b1cf425b6bb5ab4c1f70f0c
Grilindor <marcor91@hotmail.fr>
2024-02-19 13:15:30 +0000

    task5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description:
+ *with printf i need to add \n to make a new line at the end of my prints
+ *Return: tell me with a 0 if the code work
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
