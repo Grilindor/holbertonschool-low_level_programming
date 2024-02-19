5dc70e5fbcb348a5557e46497e377cafc9887ae0
Grilindor <marcor91@hotmail.fr>
2024-02-19 12:49:31 +0000

    task4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#includ.stdio.h
+/**
+ *main - entry point
+ *Description: the () are empty because i don't have any value to work with
+ *with puts i make a new line at the and of my prints
+ *return tell me with a 0 if the code work
+ */
+int main(viod)
+{
+  puts("\"Programming is like building a multilingual puzzle")
+  return (0)
+}
