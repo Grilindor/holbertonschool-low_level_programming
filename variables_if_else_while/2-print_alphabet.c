ad663193dbba1051e5e41fa1f228a0acff827ad2
Grilindor <marcor91@hotmail.fr>
2024-02-20 13:33:26 +0000

    task2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+*main - main fonction try to print this one in char
+*Description: prints the alphabet in lowercase, followed by a new line
+*Return: tell me with a 0 if the code work
+*/
+
+int main(void)
+{
+for (int i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+return (0);
+}
