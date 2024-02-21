638d25662524522bee403ade0ff3d7d87f8517e2
Grilindor <marcor91@hotmail.fr>
2024-02-21 08:57:17 +0000

    tasck4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - main fonction try to print this one in char
+*Description: prints the alphabet in lowercase, followed by a new line
+*Return: tell me with a 0 if the code work
+*/
+
+int main(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+if (letter != 'e' && letter != 'q')
+putchar(letter);
+letter++;
+}
+putchar("\n");
+return (0);
+}
