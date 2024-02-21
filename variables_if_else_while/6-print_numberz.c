c7d8820a853ffde4f6060b7f645dddf98b8225b5
Grilindor <marcor91@hotmail.fr>
2024-02-21 11:13:06 +0000

    task6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - print digit of base 10 from 0
+ *withoot all fonction excepte putchar
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
