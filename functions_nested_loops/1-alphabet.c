6de5784c76e4ab7669ba49902f8b1d57af1b95e5
Grilindor <marcor91@hotmail.fr>
2024-02-22 21:28:55 +0000

    task1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*main - main fonction
+*C: it's fonction hypothetique
+*Return: tell me with a 0 if the code work
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a', i <= 'z', i++)
+		_Putchar(i);
+	_putchar('\n');
+	return (0);
+}
