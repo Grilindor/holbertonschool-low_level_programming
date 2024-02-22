9d45b70a19a6b7af8f163e4311943f8896e83830
Grilindor <marcor91@hotmail.fr>
2024-02-22 20:32:10 +0000

    task0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,12 +3,20 @@
 #include "main.h"
 /**
 *main - main fonction  prints _putchar
+*C: it's fonction hypothetique
 *Return: tell me with a 0 if the code work
 */
-int _putchar(char c);
 
 int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-	return 0;
+	return (0);
 }
