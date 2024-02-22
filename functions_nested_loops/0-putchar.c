a1cc98d9d1ceff036847aed32d3e8cac9ab64046
Grilindor <marcor91@hotmail.fr>
2024-02-22 15:33:38 +0000

    task0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*main - main fonction  prints _putchar
+*Return: tell me with a 0 if the code work
+*/
+int _putchar(char c);
+
+int main(void)
+{
+	_putchar('\n');
+	return 0;
+}
