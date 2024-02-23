1e19c81e92e7643bea060f7d4624fc160b0ebdc7
Grilindor <marcor91@hotmail.fr>
2024-02-23 13:38:53 +0000

    task10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*print_sign - main fonction print the alphabet
+*@n: it's fonction hypothetique
+*Return: tell me with a 0 if the code work
+*/
+int add(int r, int i)
+{
+	int t;
+	
+	t = r + i;
+	return(t);
+}
