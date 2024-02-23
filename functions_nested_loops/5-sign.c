fcbf030dd3a9a348189b514c283e0da58c66647a
Grilindor <marcor91@hotmail.fr>
2024-02-23 10:19:39 +0000

    task5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*print_sign - main fonction print the alphabet
+*@n: it's fonction hypothetique
+*Return: tell me with a 0 if the code work
+*/
+int print_sign(int n)
+{
+	if (n > '0')
+	{
+		_putchar ('+');
+		return (1);
+	}
+
+	else if (n < '0')
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('\n');
+}
