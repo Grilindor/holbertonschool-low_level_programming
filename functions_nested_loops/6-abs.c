4940e94ba238cd23c39a5cfb98dfe09ea97ec29d
Grilindor <marcor91@hotmail.fr>
2024-02-23 12:53:26 +0000

    task6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*_abs - main fonction
+*@int: more shit
+*Return: tell me with a 0 if the code work
+*/
+int _abs(int)
+{
+	int r;
+
+	abs(r);
+	_putchar(r);
+	_putchar('\n');
+	return (0);
+}
