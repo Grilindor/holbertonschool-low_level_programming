6f39fdd96556363369e458f3450fc5886ec2b118
Grilindor <marcor91@hotmail.fr>
2024-02-23 08:09:21 +0000

    task1 fix

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,13 +6,14 @@
 *C: it's fonction hypothetique
 *Return: tell me with a 0 if the code work
 */
-
-int main(void)
+void print_alphabet(void)
 {
 	int i;
 
 	for (i = 'a'; i <= 'z'; i++)
+	{
 		_putchar(i);
+	}
 	_putchar('\n');
 	return (0);
 }
