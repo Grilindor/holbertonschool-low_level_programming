724234734262145eabe21fdd2d17aa2afe2a1333
Grilindor <marcor91@hotmail.fr>
2024-02-23 10:01:49 +0000

    task4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*_isalpha - main fonction if lower or uper case
+*@c: is a letter
+*Return: tell me with a 1 if the code work
+*/
+int _isalpha(int c)
+{
+	int c;
+
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
