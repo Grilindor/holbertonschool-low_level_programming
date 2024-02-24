2e0ff4fc6e4715c556231dc0da2de5e138fc42e1
Grilindor <marcor91@hotmail.fr>
2024-02-24 17:54:06 +0000

    task8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*jack_bauer - main fonction print the alphabet
+*
+*Return: tell me with a 0 if the code work
+*/
+void jack_bauer(void)
+{
+	int h1, h2, m1, m2;
+
+	for (h1 = 0; h1 <= 3; h1++)
+	{
+		for (h2 = 0; h2 < 10; h2++)
+		{
+			for (m1 = 0; m1 <= 5; m1++)
+			{
+				for (m2 = 0; m2 < 10; m2++)
+				{
+					_putchar(h1);
+					_putchar(h2);
+					_putchar(':');
+					_putchar(m1);
+					_putchar(m2);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
