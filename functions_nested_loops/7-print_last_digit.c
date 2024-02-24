d9e3f119d82730e179ae54e31ce91a4b4b8245b4
Grilindor <marcor91@hotmail.fr>
2024-02-24 15:15:20 +0000

    task7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-*print_sign - main fonction print the alphabet
-*@n: it's fonction hypothetique
+*print_last_digit - main fonction print the alphabet
+*@n: it's a value
 *Return: tell me with a 0 if the code work
 */
 int print_last_digit(int n)
 {
+	int last;
+
 	last = n % 10;
-	
+	return (last);
 }
