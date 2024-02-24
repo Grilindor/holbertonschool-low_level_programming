691d188a6e36946771c1eb198e886ecb5aa11215
Grilindor <marcor91@hotmail.fr>
2024-02-24 18:04:25 +0000

    task8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,15 +8,18 @@
 */
 void jack_bauer(void)
 {
-	int h1, h2, m1, m2;
+	int h1 = '0';
+	int h2 = '0';
+	int m1 = '0';
+	int m2 = '0';
 
-	for (h1 = 0; h1 <= 3; h1++)
+	for (h1 = '0'; h1 <= '2'; h1++)
 	{
-		for (h2 = 0; h2 < 10; h2++)
+		for (h2 = '0'; h2 <= '9'; h2++)
 		{
-			for (m1 = 0; m1 <= 5; m1++)
+			for (m1 = '0'; m1 <= '5'; m1++)
 			{
-				for (m2 = 0; m2 < 10; m2++)
+				for (m2 = '0'; m2 <= '9'; m2++)
 				{
 					_putchar(h1);
 					_putchar(h2);
