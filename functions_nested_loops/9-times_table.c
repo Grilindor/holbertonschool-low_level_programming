6757821c5b7d74547123b580829fa4a473e8710f
Grilindor <marcor91@hotmail.fr>
2024-02-25 13:30:24 +0000

    task9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,11 +10,27 @@
 */
 void times_table(void)
 {
-	int t = '0', c = '0';
+	int a, b, cal;
 
-	for (t = '0'; t <= '9'; t++)
+	for (a = '0';a <= '9'; a++)
 	{
-		_putchar(t * c);
+		_putchar('0');
+		for (b = 1; b <= 9; b++)
+		{
+			cal = a * b;
+			_putchar(' ');
+			_putchar(',');
+			if (cal <= 9)
+			{
+				_putchar(' ');
+				_putchar(op + '0');
+			}
+			else
+			{
+				_putchar((cal / 10) + '0');
+				_putchar((cal % 10) + '0');
+			}
+		}
 		_putchar('\n');
 	}
 }
