051d86cf9015b0b6ea9d1ae727e74edca6478a19
Grilindor <marcor91@hotmail.fr>
2024-02-26 10:11:16 +0000

    task2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*mul - main fonction print the alphabet
+*@a: it's variable
+*@b: it's variable
+*Return: return the t
+*/
+int mul(int a, int b)
+{
+	int t;
+
+	t = a * b;
+	return (t);
+}
