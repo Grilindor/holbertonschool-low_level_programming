93b366a1cdcfe6630d49c9278ccae3c480f4909d
Grilindor <marcor91@hotmail.fr>
2024-02-26 10:48:59 +0000

    task3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*print_numbers - print 0 to 9
+*@
+*
+*Return: tell me with a 0 if the code work
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
