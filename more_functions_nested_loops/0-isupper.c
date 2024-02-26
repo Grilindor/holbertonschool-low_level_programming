fae8664e6e008956bbc0f887d0691e34fefda38f
Grilindor <marcor91@hotmail.fr>
2024-02-26 09:26:28 +0000

    task0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*_isupper - main fonction to test if uppercase
+*@c: it's fonction hypothetique
+*Return: 1 succces
+*/
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+	_putchar('\n');
+}
