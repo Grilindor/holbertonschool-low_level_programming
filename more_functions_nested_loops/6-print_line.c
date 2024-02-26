efb68f4bfaba3c8e15729345b72607b2cc100d3a
Grilindor <marcor91@hotmail.fr>
2024-02-26 14:29:29 +0000

    task6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -3,7 +3,7 @@
 #include "main.h"
 /**
 *print_line - main fonction print number at 98
-*@y: it's a value
+*@n: it's a value
 *Return: tell me with a 0 if the code work
 */
 void print_line(int n)
@@ -17,8 +17,8 @@ void print_line(int n)
 			_putchar(95);
 			y++;
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 	else
 		_putchar('\n');
 }
