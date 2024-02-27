0a569bb0e56350ca8b429a78b81e9b940c7a78aa
Grilindor <marcor91@hotmail.fr>
2024-02-27 09:53:41 +0000

    task9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,14 +14,17 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if ((n % 15) == 0)
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		else if ((n % 3) == 0)
-			printf("Fizz");
+			printf("Fizz ");
 		else if ((n % 5) == 0)
-			printf("Buzz");
+		{
+			if (n == 100)
+				printf("Buzz\n");
+			else
+				printf("Buzz ");
 		else
 			printf("%d", n);
-		printf(" ");
 
 	}
 	return (0);
