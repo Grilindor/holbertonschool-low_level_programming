657e15ad2a9b1f769e941f8c6670cee1615559ea
Grilindor <marcor91@hotmail.fr>
2024-02-27 14:55:07 +0000

    task0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *reset_to_98 - main fontion replace var 402 to 98
+ *@n: it's a var and change the pointeur
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
