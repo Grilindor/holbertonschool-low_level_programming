bf65f605d424e0861086f475e5610ecce30d6e19
Grilindor <marcor91@hotmail.fr>
2024-02-27 21:37:33 +0000

    task6

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *_strlen - main fontion to return the max len of s
+ *@s: it's a var
+ *Return: look up
+ *
+ */
+
+int _strlen(char *s)
+{
+	int t;
+
+	t = strlen(s);
+	return (t);
+}
