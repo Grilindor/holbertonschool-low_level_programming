4ee78d92b862c9432f8c6445f75400f6115098bd
Grilindor <marcor91@hotmail.fr>
2024-02-27 12:33:07 +0000

    task10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -3,31 +3,30 @@
 #include "main.h"
 
 /**
-*print_diagonal - main fonction print number at 98
-*@n: it's a value
+*print_triangle - main fonction print number at 98
+*@size: it's a value
 *Return: tell me with a 0 if the code work
 */
 void print_triangle(int size)
 {
-        int y = 0;
-        int espa;
+	int y = 0;
+	int espa;
 
-        if (size >= 0)
-        {
-                while (y < size)
-                {
-                        for (espa = size; espa >= 0; espa--)
-                        {
-                                if (espa <= y)
-                                        _putchar('#');
-				else
-                                        _putchar(' ');
-                        }
-                        _putchar('\n');
-                        y++;
-                }
-        }
-        else if (size <= 0)
+	if (size <= 0)
                 _putchar ('\n');
-
+	else
+	{
+		while (y < size)
+		{
+			for (espa = size - 1; espa >= 0; espa--)
+			{
+				if (espa <= y)
+					_putchar('#');
+				else
+					_putchar(' ');
+			}
+			_putchar('\n');
+			y++;
+		}
+	}
 }
