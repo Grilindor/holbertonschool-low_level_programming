dd8afe84f58ee54885875b6422197429cf7fa075
Grilindor <marcor91@hotmail.fr>
2024-02-28 22:11:56 +0000

    task8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ *puts_half - main fontion it's like printf
+ *@str: what we print in last half
+ *no return
+ */
+
+void print_array(int *a, int n)
+{
+	
+}
