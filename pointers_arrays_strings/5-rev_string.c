b8c83da3c01e659cbd1eef89b24e1f5ffeeb7488
Grilindor <marcor91@hotmail.fr>
2024-02-28 12:36:56 +0000

    task5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -14,9 +14,9 @@ void rev_string(char *s)
 {
 	int i;
 
-	for (i = strlen(s) - 1; i >= 0; i--)
+	for (i = _strlen(s) - 1; i >= 0; i--)
 	{
-		putchar(s[i]);
+		_putchar(s[i]);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
