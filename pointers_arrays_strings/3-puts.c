a5a43bbb0f09873750d7210aa4469c37b14dcac8
Grilindor <marcor91@hotmail.fr>
2024-02-28 08:14:58 +0000

    task3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,12 +1,18 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- *
- *
- *
+ *_puts - main fontion it's like printf
+ *@str: what we print
+ *no return
  */
 
 void _puts(char *str)
 {
-	
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
