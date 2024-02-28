3877b9c0236dbfb337d4bae4c8f44103bd060b86
Grilindor <marcor91@hotmail.fr>
2024-02-28 20:04:05 +0000

    task7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include "main.h"
-
+#include "2-strlen.c"
 /**
- *_puts - main fontion it's like printf
- *@str: what we print
+ *puts_half - main fontion it's like printf
+ *@str: what we print in last half
  *no return
  */
 
 void puts_half(char *str)
 {
 	int len = 0;
-	
+
+	for (len = _strlen(str) / 2; len <= _strlen(str); len++)
+		_putchar(str[len]);
+	_putchar('\n');
 }
