3d1b0a42e1df109f6002e1b56062e3f438f4cefe
Grilindor <marcor91@hotmail.fr>
2024-02-29 13:51:47 +0000

    task10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *_strlen - main fontion to return the max len of s
+ *@s: it's a var
+ *Return: look up
+ *
+ */
+
+int _atoi(char *s)
+	
