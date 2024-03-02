78868ddb9abfe55c752e055e0c38d727c493ca0f
Grilindor <marcor91@hotmail.fr>
2024-03-02 16:12:06 +0000

    task 3-6 ?

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+#include "2-strlen.c"
+/**
+ *cap_string - main fontion make hupper 1er letter of word
+ *@str: the array
+ *
+ *
+ *
+ *Return: renvoie le str
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; i <= _strlen(str); i++)
+	{
+		if (str[i] == 9 || str[i] == 32 || str[i] == '\n' || str[i] == 46)
+		{
+			i++;
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+				continue;
+			}
+			i--;
+		}
+	}
+	return (str);
+}
