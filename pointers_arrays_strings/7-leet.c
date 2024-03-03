5daae0d1af9f5e1a7298e0483c165542bcb64cf6
Grilindor <marcor91@hotmail.fr>
2024-03-03 18:11:06 +0000

    task3-7 a finir

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include "2-strlen.c"
+/**
+ *string_toupper - main fontion it's like printf in revers
+ *@str: the array str we change
+ *Return: of char str
+ */
+
+char *leet(char *str)
+{
+	int upper_case[] = {65, 69, 79, 84, 76};
+	int lower_case[] = {97, 101, 111, 116, 108};
+	int relace[] = {52, 51, 48, 55, 49};
+	*r = *str;
+	int i;
+
+	for (i = 0; i <= _strlen(str); i++)
+	{
+		
+	}
+	return (r);
+}
