00d80c81f281f561cb57ab6b7139e9c6ae5a02a7
Grilindor <marcor91@hotmail.fr>
2024-03-04 15:22:41 +0000

    task 1-3 a finir

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include "2-strlen.c"
+/**
+ *leet - main fontion it's like printf in revers
+ *@str: the array str we change
+ *Return: of char str
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, cont = 0;
+
+	for (i = O; _strchr(*s, *accept) < '\0'; i++)
+	{
+		
+	}
+}
