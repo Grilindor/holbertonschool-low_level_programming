df961ff00ae6c6cb878087b0f3f1855b4c525a08
Grilindor <marcor91@hotmail.fr>
2024-03-06 08:27:59 +0000

    task 1-4fix

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -18,7 +18,7 @@ char *_strstr(char *haystack, char *needle)
 		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (*haystack == *needle)
-				return (&haystack[i]);
+				return (&haystack[i - 1]);
 			haystack++;
 		}
 	}
