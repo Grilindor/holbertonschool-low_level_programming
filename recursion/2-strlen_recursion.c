0d536e9ff739fb110315243d2f034294a1f9d796
Grilindor <marcor91@hotmail.fr>
2024-03-06 15:00:05 +0000

    task2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ *_strlen_recursion - main fontion that returns the length of a string
+ *@s: it's first array
+ *
+ *Return: return the total of byte
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
