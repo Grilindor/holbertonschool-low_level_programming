81ca400917ef93d4449bd6b477e82e711a83a5f6
Grilindor <marcor91@hotmail.fr>
2024-03-07 21:05:01 +0000

    task5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -5,21 +5,25 @@
 /**
  *_pow_recursion - main fontion check for the squar of n
  *@n: it's the numbers
- *@
- *faire 2 fonction 1 pour la fonction et l'autre pour aider la fontion
+ *@y: the initial of n (sere de reference) fonction 1 pour la fonction et
+ * l'autre pour aider la fontion
+ *_sqrt_recursion - fonction return n
  *Return: return
  */
 
 int _pow_recursion(int n, int y)
 {
-        if (n * n == y)
-                return (n);
-        else if (n * n > y)
-                return (-1);
-
-        return (_pow_recursion(n + 1, y));
+	if (n * n == y)
+		return (n);
+	else if (n * n > y)
+		return (-1);
+	return (_pow_recursion(n + 1, y));
 }
-
+/**
+ *_sqrt_recursion - fonction return n
+ *@n: int
+ *Return: return
+ **/
 
 int _sqrt_recursion(int n)
 {
