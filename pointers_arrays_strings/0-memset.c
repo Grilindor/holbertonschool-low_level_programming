eb0119b32b03232b8cbda88916636826fdc932bc
Grilindor <marcor91@hotmail.fr>
2024-03-07 08:47:53 +0000

    task0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,19 +2,20 @@
 #include "main.h"
 #include "2-strlen.c"
 /**
- *cap_string - main fontion make hupper 1er letter of word
- *@str: the array
+ *_memset - main fontion make hupper 1er letter of word
+ *@s: the array
+ *@b: the byte
+ *@n: the longueur we fill
  *
- *
- *
- *Return: renvoie le str
+ *Return: renvoie le s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++) {
+	for (i = 0; i < n; i++)
+	{
 		s[i] = b;
 	}
 	return (s);
