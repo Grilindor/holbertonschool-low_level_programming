e90016f72bfb030906279ebcacd5ecae5c919309
Grilindor <marcor91@hotmail.fr>
2024-03-09 10:31:43 +0000

    task3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  *main - a program that prints multiplies two numbers
  *@argc: is de commande line
@@ -8,18 +8,20 @@
  *Return: return 0 if success
  */
 
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
-	int i;
+	int i, total = 0;
 
-	if (argc < 2)
+	if (argc < 3)
 	{
 		printf("ERROR\n");
 		return (1);
 	}
 	else
 		for (i = 1; i < argc; i++)
-			printf(argv[1] * argv[2]);
-	printf("\n");
+		{
+			total = atoi(argv[1]) * atoi(argv[2]);
+		}
+	printf("%d\n", total);
 	return (0);
 }
