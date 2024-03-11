e06cbeea3da1ad36ea3bfe22ae6aba2e3579d8a6
Grilindor <marcor91@hotmail.fr>
2024-03-11 21:59:55 +0000

    task3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "_putchar.c"
+/**
+ * alloc_grid - main function function that returns a pointer
+ * to a 2 dimensional array of integers
+ * space in memory, which contains a copy of the string given
+ * as a parameter
+ * @width: the number of ligne
+ *@height: the number of colonne
+ *
+ * Return: return a pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **new_array;
+	int i, l;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	new_array = malloc(width * sizeof(int *));
+	if (new_array == NULL)
+	{
+		free(new_array);
+		return (NULL);
+	}
+	for (i = 0; i < width; i++)
+	{
+		new_array[i] = malloc(height * sizeof(int));
+	}
+	for (i = 0; i < width; i++)
+	{
+		for (l = 0; l < height; l++)
+			new_array[i][l] = 0;
+	}
+	if (new_array == NULL)
+		return (NULL);
+	return (new_array);
+}
