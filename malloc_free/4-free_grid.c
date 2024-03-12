b73291e8c2ccbc71b9e3dc82188ba187c07a911a
Grilindor <marcor91@hotmail.fr>
2024-03-12 09:08:06 +0000

    task4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * free_grid - main function that frees a 2 dimensional grid
+ * previously created by your alloc_grid function
+ * @grid: the array we will free
+ *@height: sa taille
+ *
+ * Return: return a pointer to the duplicated string
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i)
+		free(grid[i]);
+	free(grid);
+}
