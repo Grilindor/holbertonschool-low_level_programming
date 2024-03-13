0482d9f593cb765bf452fa398cde15c8ca8e9cc3
Grilindor <marcor91@hotmail.fr>
2024-03-13 15:01:47 +0000

    task1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,9 +16,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *new_str;
 
 	if (s1 == NULL)
-		s1 = NULL;
+		s1 = "";
 	if (s2 == NULL)
-		s2 = NULL;
+		s2 = "";
 
 	for (l = 0; s1[l] != '\0'; l++)
 		;
