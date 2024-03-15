686ca58157453f2e70410b7de758f009a69f5c0b
Grilindor <marcor91@hotmail.fr>
2024-03-15 09:30:45 +0000

    task0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,16 @@
-/**Define a new type struct dog with the following elements**/
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - dog data
+ *@name: name of the dog
+ * @age: it's age what did you expect
+ *@owner: name of owner
+ *Define a new type struct dog with the following elements
+**/
 struct dog
 {
 	char *name;
 	char *owner;
 	float age;
 };
+#endif
