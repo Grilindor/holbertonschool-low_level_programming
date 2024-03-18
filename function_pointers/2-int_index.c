f6c3dfe82647e54c6e9a5a48bdb8ecfd88c3e877
Grilindor <marcor91@hotmail.fr>
2024-03-18 14:15:54 +0000

    task2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - a function that executes a function given
+ * as a parameter on each element of an array
+ * @array: the array
+ * @size: is the size of the array
+ * @cmp: is a pointer to the function to be used to compare values
+ * Return: returns the index of the first element for which the cmp function
+ * does not return 0
+ **/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, y;
+
+	y = size;
+
+	if (y <= 0)
+		return (-1);
+	for (i = 0; i < y; i++)
+	{
+		cmp(array[i]);
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
