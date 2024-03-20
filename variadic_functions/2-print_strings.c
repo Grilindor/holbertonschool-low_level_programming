0ad5d2d3fe7ee045bd7c1d472dbecddfe47aae0d
Grilindor <marcor91@hotmail.fr>
2024-03-20 13:34:35 +0000

    task2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -3,31 +3,30 @@
 #include <stdarg.h>
 
 /**
- * array_iterator - a function that executes a function given
- * as a parameter on each element of an array
- * @array: the array
- * @size: is the size of the array
- * @action: executes a function
+ * print_strings - function that prints strings, followed by a new line
+ * @separator: the string to be printed between the strings
+ * @n: the number of strings passed to the function
  * Return: void.
  **/
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-        va_list prstring;
+	va_list prstring;
 
-        va_start(prstring, n);
-        for (i = 0; i < n; i++)
-        {
+	va_start(prstring, n);
+	for (i = 0; i < n; i++)
+	{
 		const char *str = va_arg(prstring, const char *);
+
 		if (str == NULL)
 			printf("(nil)");
-                else
+		else
 			printf("%s", str);
-                if (i != (n - 1) && separator != NULL)
-                        printf("%s", separator);
-        }
-        printf("\n");
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
 
-        va_end(prstring);
+	va_end(prstring);
 }
