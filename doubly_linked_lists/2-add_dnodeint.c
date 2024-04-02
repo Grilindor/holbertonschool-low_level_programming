cea1209701fc5c53859181c9453de540f3d4dcd6
Grilindor <marcor91@hotmail.fr>
2024-04-02 09:27:00 +0000

    adds a new node at the beginning of a list, normaly its work

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+*add_dnodeint - a function that adds a new node at the beginning of a list
+*@head: the 1er node of the list
+*@n: the integer
+*Return: return the new head of the list and the len
+**/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	int i;
+	dlistint_t *new_node;
+	/*alloing memori for new node*/
+	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	/*succes of malloc?*/
+	if (new_node == NULL)
+		return (NULL);
+
+	for (i = 0; i < n; i++)
+		;
+	new_node->n = i;/*length of integer, n*/
+
+	/*the new head point to the old head*/
+	new_node->next = (*head);
+
+	/*repeat the processus*/
+	(*head) = new_node;
+	return (new_node);
+}
