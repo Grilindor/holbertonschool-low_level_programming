9a51d45d90eb7cec9f30ea24ad1465cee0d92a0d
Grilindor <marcor91@hotmail.fr>
2024-04-03 14:08:04 +0000

    task4

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -2,17 +2,18 @@
 #include <stdlib.h>
 
 /**
+* free_list -  a function that frees a list
+*@head: the head of the list
 *
-*
-*
-*
+*Return: it's void
 **/
 
 void free_list(list_t *head)
 {
 	while (head != NULL)
 	{
-		head = head->next;
+		free(head->str);
 		free(head);
+		head = head->next;
 	}
 }
