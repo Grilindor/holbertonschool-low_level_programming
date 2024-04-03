b77b73c4121ab94502a463d8aca13f44d230575a
Grilindor <marcor91@hotmail.fr>
2024-04-03 13:08:17 +0000

    task0 add comment

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,13 +12,17 @@ size_t print_list(const list_t *h)
 {
 	unsigned int i, len = 0;
 
+	/* Traverse the list until we reach the end */
 	for (i = 0; h != NULL; i++)
 	{
+		/* Check if the string is NULL */
 		if (h->str == NULL)
-			printf("[%d] (nil)\n", len);
+			printf("[0] (nil)\n");
 		else
+			/* Print the length and string */
 			printf("[%d] %s\n", h->len, h->str);
 		len++;
+		/* Move to the next element of the list */
 		h = h->next;
 	}
 	return (len);
