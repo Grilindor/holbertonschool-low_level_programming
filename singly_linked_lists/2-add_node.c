9ac2523c409aa4dd616818168f694b823c57acac
Grilindor <marcor91@hotmail.fr>
2024-04-03 13:15:11 +0000

    remplace the strlen by a loop, WHYYYY?

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,6 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	/*alloing memori for new node*/
 	list_t *new_node = malloc(sizeof(list_t));
+	int i;
 
 	/*succes of malloc?*/
 	if (new_node == NULL)
@@ -21,7 +22,9 @@ list_t *add_node(list_t **head, const char *str)
 	/*Duplicates string*/
 	new_node->str = strdup(str);
 
-	new_node->len = strlen(str);/*length of string, str*/
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	new_node->len = i;/*length of string, str*/
 
 	/*the new head point to the old head*/
 	new_node->next = (*head);
