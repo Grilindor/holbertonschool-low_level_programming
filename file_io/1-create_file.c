3ca86f87a9e3191c41cdde81eef5d8826b2a48fd
Grilindor <marcor91@hotmail.fr>
2024-04-07 16:14:23 +0000

    task1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - Function to create a new file with
+ * specified filename and content
+ *@filename: Name of the file to be created
+ *@text_content: Content to be written to the file
+ *Returns: - 1 on success, -1 on failure
+ **/
+
+int create_file(const char *filename, char *text_content)
+{
+	int i, new_file;
+
+	/*Check if filename is NULL*/
+	if (filename == NULL)
+		return (-1);
+
+	/*If text_content is provided, set it to an empty string*/
+	if(text_content)
+		text_content = "";
+
+	/*Calculate the length of the text_content*/
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+
+	/*Create a new file with specified permissions*/
+	new_file = open(filename, O_CREAT | O_WRONLY |O_TRUNC, 0600);
+
+	if (new_file == -1)
+		return (-1);
+
+	write(new_file, text_content, i);
+
+	return (1);
+}
