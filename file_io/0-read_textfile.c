a26a069bf33ebb832385fc7ab4673e70473e88e7
Grilindor <marcor91@hotmail.fr>
2024-04-07 10:05:34 +0000

    task0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,9 +3,9 @@
 /**
  * read_textfile - a function that reads a text file and prints it
  * to the POSIX standard output
- *@filename: 
- *@letters:
- *Return: 
+ *@filename: the text
+ *@letters: the len of the string
+ *Return: the text
  **/
 
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -15,17 +15,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	/*Allocate memory for the text buffer*/
 	text = malloc(letters);
-	if (text == NULL) return (0);
+	if (text == NULL)
+		return (0);
 
 	/*Check if the filename is valid*/
-	if (filename == NULL) return (0);
+	if (filename == NULL)
+		return (0);
 
 	/*Open the file in read-only mode*/
 	file = open(filename, O_RDONLY);
-	if (file == -1) {
-		free(text);
-		return (0);
-	}
+		if (file == -1)
+		{
+			free(text);
+			return (0);
+		}
 
 	/*Read 'letters' number of bytes from the file into the text buffer*/
 	let = read(file, text, letters);
