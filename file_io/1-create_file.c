a1d87bdb126e7e139a5edae51b81fa74db6b3b95
Grilindor <marcor91@hotmail.fr>
2024-04-07 16:24:29 +0000

    task1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	/*If text_content is provided, set it to an empty string*/
-	if (text_content)
+	if (text_content == NULL)
 		text_content = "";
 
 	/*Calculate the length of the text_content*/
